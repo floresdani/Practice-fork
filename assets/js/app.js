abab75464c4ca5c4abad164226201bd3342e165f
floresdani <florescaceresdaniela@gmail.com>
2018-05-22 11:53:02 -0400

    aqui bien crazy subiendo el ejercicio

---
+++ assets/js/app.js
@@ -0,0 +1,17 @@
+window.onload = function () {
+    var btn = document.getElementById("button-change");
+    var txt = document.getElementById("paragraph");
+    var btnDos = document.getElementById("magic");
+    var txtDos = document.getElementById("text");
+
+btn.addEventListener("click", function(){
+    
+    txt.innerHTML = "<h2>Git & Github TE AMAMOS<h2>";
+})
+
+btnDos.addEventListener("click", function() {
+//txtDos.innerHTML = "";
+txtDos.classList.add("disappear");
+})
+
+}
